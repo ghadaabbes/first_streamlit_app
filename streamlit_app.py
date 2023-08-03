5bafef83b19a2c2884e3bae8486efae89ae4b5d0
ghadaabbes <36130489+ghadaabbes@users.noreply.github.com>
2023-08-03 16:40:30 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit 
 import pandas
+import requests
+
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
@@ -18,3 +20,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 #display the table here
 streamlit.dataframe(fruits_to_show)
 
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
