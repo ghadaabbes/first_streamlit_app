9a803f53482e92feae85ad1371f5a12894d67479
ghadaabbes <36130489+ghadaabbes@users.noreply.github.com>
2023-08-07 16:09:31 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,15 +24,24 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
 streamlit.header("Fruityvice Fruit Advice!")
-# we get JSON data normalized
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# display table here
-streamlit.dataframe(fruityvice_normalized)
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("please select a fruit to get information.")
+  else:
+    #imports requests
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
+    streamlit.write('Thanks for adding', fruit_choice)
+    # we get JSON data normalized
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # display table here
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
+
+#streamlit.write('The user entered ', fruit_choice)
 
 streamlit.text("Hello from Snowflake:")
 
@@ -52,5 +61,5 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 fruit_choice = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.text("Thanks for adding jackfruit")
-streamlit.write('Thanks for adding', fruit_choice)
+
 my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlite')")
