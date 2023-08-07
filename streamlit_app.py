64addff53e576454d9fe7c32f4464c081e91877e
ghadaabbes <36130489+ghadaabbes@users.noreply.github.com>
2023-08-07 16:26:21 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,13 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 #display the table here
 streamlit.dataframe(fruits_to_show)
-
+def get_fruityvice_data(this_fruit_choice):
+  #imports requests
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
+  streamlit.write('Thanks for adding', fruit_choice)
+  # we get JSON data normalized
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
 
 #streamlit.text(fruityvice_response)
 streamlit.header("Fruityvice Fruit Advice!")
@@ -31,13 +37,10 @@ try:
   if not fruit_choice:
     streamlit.error("please select a fruit to get information.")
   else:
-    #imports requests
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
-    streamlit.write('Thanks for adding', fruit_choice)
-    # we get JSON data normalized
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    #call the function
+    back_from_function = get_fruityvice_data(fruit_choice)
     # display table here
-    streamlit.dataframe(fruityvice_normalized)
+    streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()
 
