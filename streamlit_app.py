5fdd14c277296cc7de7924633d86f6bc9d108f75
ghadaabbes <36130489+ghadaabbes@users.noreply.github.com>
2023-08-07 18:49:27 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -73,18 +73,18 @@ if streamlit.button('Get Fruit Load List'):
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
   
-streamlit.text("Hello from Snowflake:")
+#streamlit.text("Hello from Snowflake:")
 #streamlit.text(my_data_rows)
-my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_row2 = my_cur.fetchone()
-streamlit.text("the fruit load list contains:")
+#my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+#my_data_row2 = my_cur.fetchone()
+#streamlit.text("the fruit load list contains:")
 
 
-streamlit.text(my_data_row2)
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
-fruit_choice = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.text("Thanks for adding jackfruit")
+#streamlit.text(my_data_row2)
+#my_data_rows = my_cur.fetchall()
+#streamlit.header("The fruit load list contains:")
+#streamlit.dataframe(my_data_rows)
+#fruit_choice = streamlit.text_input('What fruit would you like to add?','jackfruit')
+#streamlit.text("Thanks for adding jackfruit")
 
 
